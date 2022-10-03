0c25d079f7c2d5d1b7434ac8e6ac542104795c80
Kyle Jones <kyle@kylejones.dev>
2022-10-03 12:18:15 -0700

    Fix hamburger button not responding

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,18 +1,20 @@
-const mobileButtonHamburger = document.getElementById('hamburger-button')
-const mobileButtonX = document.getElementById('x-button')
-const mobileMenu = document.getElementById('mobile-menu')
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileSymbolHamburger = document.getElementById('hamburger-symbol');
+const mobileSymbolX = document.getElementById('x-symbol');
+const mobileMenu = document.getElementById('mobile-menu');
 
-const hamburgerChange = () => {
-    mobileButtonHamburger.classList.add('hidden');
-    mobileButtonX.classList.remove('hidden');
-    mobileMenu.classList.remove('hidden');
+const hamburgerToggle = () => {
+    if(!mobileSymbolHamburger.classList.contains('hidden')) {
+        mobileSymbolHamburger.classList.add('hidden');
+        mobileSymbolX.classList.remove('hidden');
+        mobileMenu.classList.remove('hidden');
+    } else if (mobileSymbolHamburger.classList.contains('hidden')) {
+        mobileSymbolHamburger.classList.remove('hidden');
+        mobileSymbolX.classList.add('hidden');
+        mobileMenu.classList.add('hidden');
+    } else {
+        console.log("Mobile menu button exception.")
+    }
 }
 
-const xChange = () => {
-    mobileButtonX.classList.add('hidden');
-    mobileButtonHamburger.classList.remove('hidden');
-    mobileMenu.classList.add('hidden');
-}
-
-mobileButtonHamburger.addEventListener('click', hamburgerChange);
-mobileButtonX.addEventListener('click', xChange);
+mobileMenuButton.addEventListener('click', hamburgerToggle);
